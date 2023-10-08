44cb6affc96d6a0028f6ae856d5f1a5f73fb594d
chana1024 <linzhinan1024@gmail.com>
2023-10-08 17:35:23 +0800

    a

---
+++ cerebro-google-definition/src/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import Preview from './Preview'
+import iconPath from './icon.png'
+const childProcess = require('child_process');
+const id = 'google defenition'
+export const order = 1
+export const icon = iconPath
+const REGEXP = /ge\s(.*)/;
+
+const queryFromTerm = term => {
+  const match = term.match(/^ge (.+)$/)
+  return match ? match[1].trim() : null
+};
+/**
+ * Search term in google
+ *
+ * @param  {String} options.term
+ * @param  {Object} options.actions
+ * @param  {Function} options.display
+ */
+export const fn = ({ term, actions, display }) => {
+  const match = term.match(REGEXP);
+    if (match) {
+      const query = queryFromTerm(term)
+          /**
+           * Open browser with google search of term
+           * @param  {String} searchTerm
+           */
+          // eslint-disable-next-line no-var
+          var search = (searchTerm) => {
+            searchTerm = "google definition "+searchTerm
+            const q = encodeURIComponent(searchTerm)
+            const url = `https://www.google.com/search?q=${q}&cad=h`
+            childProcess.exec(`firefox ${url}`)
+          }
+
+          display({
+            id,
+            icon,
+            order,
+            title: `Search google definition  for ${query}`,
+            onSelect: () => search(query),
+            // getPreview: () => <Preview query={query} key={query} search={search} />
+              getPreview: () => `<div>${query}</div>`
+          })
+    }
+}
